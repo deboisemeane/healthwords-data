b7b37327c129936e7172c512058c1dcd451a884b
Alex Cook <alexander.cook@healthwords.ai>
2025-02-04 18:15:53 +0000

    Added confusion matrix.

--- signups_logreg.py
+++ signups_logreg.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix, ConfusionMatrixDisplay
 
 # Load preprocessed data
 df = pd.read_csv("data/preprocessed/preprocessed_signups.csv")
@@ -17,7 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 
 # Train logistic regression model
-model = LogisticRegression(class_weight='balanced')
+model = LogisticRegression(class_weight='balanced', penalty='l2')
 model.fit(X_train, y_train)
 
 # Predictions
@@ -27,7 +28,8 @@ y_prob = model.predict_proba(X_test)[:, 1]
 # Evaluate
 print(classification_report(y_test, y_pred))
 print("AUC Score:", roc_auc_score(y_test, y_prob))
-
+ConfusionMatrixDisplay.from_predictions(y_test, y_pred)  # Create and display in one line
+plt.show() 
 # Feature importance (coefficients)
 feature_importance = pd.DataFrame({'Feature': X.columns, 'Coefficient': model.coef_[0]})
 print(feature_importance.sort_values(by="Coefficient", ascending=False))
