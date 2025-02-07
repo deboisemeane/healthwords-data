0aff80d1077416b09128ff0aca028b238febf162
Alex Cook <alexander.cook@healthwords.ai>
2025-02-07 18:08:23 +0000

    Added landing page type feature.

--- preprocessing/signups_preprocess.py
+++ preprocessing/signups_preprocess.py
@@ -9,9 +9,13 @@ encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
 encoded_traffic = encoder.fit_transform(df[['first_traffic_channel_group']])
 encoded_traffic_df = pd.DataFrame(encoded_traffic, columns=encoder.get_feature_names_out(['first_traffic_channel_group']))
 
-# Drop the original column and replace with encoded features
-df = df.drop(columns=['first_traffic_channel_group', 'user_pseudo_id'])
-df = pd.concat([df, encoded_traffic_df], axis=1)
+# One-hot encode the first_page_location
+encoded_page_location = encoder.fit_transform(df[['landing_page_category']])
+encoded_page_location_df = pd.DataFrame(encoded_page_location, columns=encoder.get_feature_names_out(['landing_page_category']))
+
+# Drop the original columns and replace with encoded features
+df = df.drop(columns=['first_traffic_channel_group', 'landing_page_category', 'user_pseudo_id'])
+df = pd.concat([df, encoded_traffic_df, encoded_page_location_df], axis=1)
 
 # Standardise numerical columns
 scaler = RobustScaler()
